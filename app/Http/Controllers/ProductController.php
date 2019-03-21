2d286cd4e2ab7fae7d6fc686bffff485eb921b2f
Mohammed Al-Sultani <mohammed>
2019-03-21 14:00:16 -0700

    Product Resource Created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -14,7 +14,8 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
+
     }
 
     /**
@@ -46,7 +47,9 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        //
+        return $product;
+
+        return new ProductResource($product);
     }
 
     /**
